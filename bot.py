76304511e864394a9fe5f2c7a37848946cc025f7
sumit <sksumitkumar003@gmail.com>
2018-11-17 18:55:01 +0530

    training DNN

--- bot.py
+++ bot.py
@@ -13,9 +13,9 @@ lr = 1e-3
 #environment
 env = gym.make('CartPole-v0')
 env.reset()
-goal_steps = 200
+goal_steps = 500
 score_requirement = 50
-intial_games = 10000
+initial_games = 10000
 
 def random_games():
 	#Each of this is its own game
@@ -42,7 +42,7 @@ def initial_population():
 	#just the scores that met our threshold
 	accepted_scores = []
 	#Iterating through no of games
-	for _ in range(intial_games):
+	for _ in range(initial_games):
 		score = 0
 		#move specifically from this environment:
 		game_memory = []
@@ -75,8 +75,8 @@ def initial_population():
 				elif data[1] == 0:
 					output = [1,0]
 
-			#saving our training data
-			training_data.append([data[0],output])
+				#saving our training data
+				training_data.append([data[0],output])
 
 		#reset to play again
 		env.reset()
@@ -95,3 +95,43 @@ def initial_population():
 
 	return training_data
 
+#simple multilayer perceptron model
+def neural_network_model(input_size):
+
+    network = input_data(shape=[None, input_size, 1], name='input')
+
+    network = fully_connected(network, 128, activation='relu')
+    network = dropout(network, 0.8)
+
+    network = fully_connected(network, 256, activation='relu')
+    network = dropout(network, 0.8)
+
+    network = fully_connected(network, 512, activation='relu')
+    network = dropout(network, 0.8)
+
+    network = fully_connected(network, 256, activation='relu')
+    network = dropout(network, 0.8)
+
+    network = fully_connected(network, 128, activation='relu')
+    network = dropout(network, 0.8)
+
+    network = fully_connected(network, 2, activation='softmax')
+    network = regression(network, optimizer='adam', learning_rate=lr, loss='categorical_crossentropy', name='targets')
+    model = tflearn.DNN(network, tensorboard_dir='log')
+
+    return model
+
+
+def train_model(training_data, model=False):
+
+    X = np.array([i[0] for i in training_data]).reshape(-1,len(training_data[0][0]),1)
+    y = [i[1] for i in training_data]
+
+    if not model:
+        model = neural_network_model(input_size = len(X[0]))
+    
+    model.fit({'input': X}, {'targets': y}, n_epoch=5, snapshot_step=500, show_metric=True, run_id='openai_learning')
+    return model
+
+training_data = initial_population()
+model = train_model(training_data)
